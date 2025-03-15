5df3fae5520fbd3f11b65278b58c7749d9b84513
Geethasri-111 <61574935+Geethasri-111@users.noreply.github.com>
2025-03-15 21:51:08 +0530

    banner commit

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -6,17 +6,25 @@ export default function decorate(block) {
     const picWrapper = document.createElement('div');  
     picWrapper.className = 'pic-overlay'; 
     [...block.children].forEach((row) => {
-        const content = document.createElement('div');           
-            while (row.firstElementChild) content.append(row.firstElementChild);
-                [...content.children].forEach((div) => {
-                if (div.children.length === 1 && (div.querySelector('picture') || div.querySelector('p picture'))) div.className = 'banner-image';
-                else div.className = 'banner-content';
-                });
-                picWrapper.append(content);
+        [...row.children].forEach((col) => {
+        const imageDiv = col.querySelector('picture');
+        imageDiv.parentNode.className = 'banner-image';
+        const content = document.createElement('div');
+        content.className = 'banner-content';   
+        const bannerDiv = imageDiv.parentNode.siblings;
+        content.append(bannerDiv);   
+        picWrapper.append(content);         
+            // while (row.firstElementChild) content.append(row.firstElementChild);
+            //     [...content.children].forEach((div) => {
+            //     if (div.children.length === 1 && (div.querySelector('picture') || div.querySelector('p picture'))) div.className = 'banner-image';
+            //     else div.className = 'banner-content';
+            //     });
+            //     picWrapper.append(content);
             // if(picWrapper){
             //     bannerContent.classList.add('banner-content');
             //     picWrapper.parentNode.insertBefore(bannerContent,picWrapper.nextSibling);
             // }
       });
+    })
   }
   
