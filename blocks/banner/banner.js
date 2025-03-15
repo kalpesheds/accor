876d47c9fb376cc012d3cc3d0f47df5be246184f
Geethasri-111 <61574935+Geethasri-111@users.noreply.github.com>
2025-03-15 17:16:42 +0530

    changes

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -3,15 +3,16 @@ export default function decorate(block) {
     block.classList.add(`banner-${cols.length}-cols`);
   
     // setup image columns
+    const picWrapper = row.createElement('div');  
+    picWrapper.className = 'pic-overlay'; 
     [...block.children].forEach((row) => {
-          const picWrapper = row.createElement('div');          
-            picWrapper.className = 'pic-overlay';
-            while (row.firstElementChild) picWrapper.append(row.firstElementChild);
-                [...picWrapper.children].forEach((div) => {
-                if (div.children.length === 1 && div.querySelector('picture')) div.className = 'banner-image';
+        const content = document.createElement('div');           
+            while (row.firstElementChild) content.append(row.firstElementChild);
+                [...content.children].forEach((div) => {
+                if (div.children.length === 1 && (div.querySelector('picture') || div.querySelector('p picture'))) div.className = 'banner-image';
                 else div.className = 'banner-content';
                 });
-                picWrapper.append(li);
+                picWrapper.append(content);
             // if(picWrapper){
             //     bannerContent.classList.add('banner-content');
             //     picWrapper.parentNode.insertBefore(bannerContent,picWrapper.nextSibling);
