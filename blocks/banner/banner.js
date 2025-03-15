3094b46cfd3bdc8f3cb25c8545932c0fd7ada69d
Geethasri-111 <61574935+Geethasri-111@users.noreply.github.com>
2025-03-15 16:58:29 +0530

    banner card changes

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -5,16 +5,19 @@ export default function decorate(block) {
     // setup image columns
     [...block.children].forEach((row) => {
       [...row.children].forEach((col) => {
-        const pic = col.querySelector('picture');
-        if (pic) {
-          const picWrapper = pic.closest('div');          
+          const picWrapper = col.createElement('div');          
             picWrapper.classList.add('pic-overlay');
             const bannerContent= document.createElement('div');
-            if(picWrapper){
-                bannerContent.classList.add('banner-content');
-                picWrapper.parentNode.insertBefore(bannerContent,picWrapper.nextSibling);
-            }
-        }
+            while (col.firstElementChild) bannerContent.append(row.firstElementChild);
+                [...bannerContent.children].forEach((div) => {
+                if (div.children.length === 1 && div.querySelector('picture')) div.className = 'banner-image';
+                else div.className = 'banner-content';
+                });
+                picWrapper.append(li);
+            // if(picWrapper){
+            //     bannerContent.classList.add('banner-content');
+            //     picWrapper.parentNode.insertBefore(bannerContent,picWrapper.nextSibling);
+            // }
       });
     });
   }
